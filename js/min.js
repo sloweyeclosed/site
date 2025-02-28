3226f493ed6134762335c85933163be9aa3a76dd
sloweyeclosed <sloweyeclose@gmail.com>
2025-03-01 00:04:04 +0200

    Add files via upload

---
+++ js/min.js
@@ -0,0 +1,7 @@
+function go() {
+  let video = document.getElementById("scr");
+  let button = document.getElementById("btn");
+  video.play();
+  video.style.display = "block";
+  button.style.display = "none";
+}
