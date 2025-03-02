1496b090d8056fc3509930429197017813599279
sloweyeclosed <sloweyeclose@gmail.com>
2025-03-02 12:57:11 +0200

    Add files via upload

---
+++ js/min.js
@@ -0,0 +1,27 @@
+function go() {
+  let video = document.getElementById("scr");
+  let google = document.getElementById("google");
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) {
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
+  }
+
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) {
+    video.msRequestFullscreen();
+  }
+  video.play();
+  video.style.display = "block";
+
+  google.style.display = "none";
+}
