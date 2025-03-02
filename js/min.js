8d9bd872e429ada411f7b9bbcb737518f1a02988
sloweyeclosed <sloweyeclose@gmail.com>
2025-03-02 11:13:41 +0200

    fullscreen

--- js/min.js
+++ js/min.js
@@ -1,6 +1,15 @@
 function go() {
   let video = document.getElementById("scr");
   let button = document.getElementById("btn");
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) {
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
+  }
   video.play();
   video.style.display = "block";
   button.style.display = "none";
