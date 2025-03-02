34fe3d1f211331b5ae89d02a89e0b0328e27e8e7
sloweyeclosed <sloweyeclose@gmail.com>
2025-03-02 12:29:33 +0200

    fullscreenашч

--- js/min.js
+++ js/min.js
@@ -2,25 +2,25 @@ function go() {
   let video = document.getElementById("scr");
   let button = document.getElementById("btn");
   let google = document.getElementById("google");
-  if (document.documentElement.requestFullscreen) {
-    document.documentElement.requestFullscreen();
-  } else if (document.documentElement.mozRequestFullScreen) {
-    document.documentElement.mozRequestFullScreen();
-  } else if (document.documentElement.webkitRequestFullscreen) {
-    document.documentElement.webkitRequestFullscreen();
-  } else if (document.documentElement.msRequestFullscreen) {
-    document.documentElement.msRequestFullscreen();
-  }
+  // if (document.documentElement.requestFullscreen) {
+  //   document.documentElement.requestFullscreen();
+  // } else if (document.documentElement.mozRequestFullScreen) {
+  //   document.documentElement.mozRequestFullScreen();
+  // } else if (document.documentElement.webkitRequestFullscreen) {
+  //   document.documentElement.webkitRequestFullscreen();
+  // } else if (document.documentElement.msRequestFullscreen) {
+  //   document.documentElement.msRequestFullscreen();
+  // }
 
-  if (video.requestFullscreen) {
-    video.requestFullscreen();
-  } else if (video.mozRequestFullScreen) {
-    video.mozRequestFullScreen();
-  } else if (video.webkitRequestFullscreen) {
-    video.webkitRequestFullscreen();
-  } else if (video.msRequestFullscreen) {
-    video.msRequestFullscreen();
-  }
+  // if (video.requestFullscreen) {
+  //   video.requestFullscreen();
+  // } else if (video.mozRequestFullScreen) {
+  //   video.mozRequestFullScreen();
+  // } else if (video.webkitRequestFullscreen) {
+  //   video.webkitRequestFullscreen();
+  // } else if (video.msRequestFullscreen) {
+  //   video.msRequestFullscreen();
+  // }
   video.focus();
   video.muted = false;
   video.play();
